50001c0c061d1f8177b159e29fc43bcebe5f6d58
shin-SeungHun <sng7387@gmail.com>
2023-12-02 23:21:37 +0900

    연습문제 2-2 생성자 추가

--- lib/11_30/cleric.dart
+++ lib/11_30/cleric.dart
@@ -5,14 +5,21 @@ import 'dart:math';
 ///  mp와 최대 mp는 정수로 초기치 10으로 한다
 ///  최대 hp와 최대 mp는 상수 필드로 선언한다.
 class Cleric {
+  /// 모든 성직자의 최대 hp, mp는 50, 10으로 정해져 있어
+  /// 각 인스턴스가 각각의 정보를 가지는 것은 메모리 낭비이다.
+  /// 따라서 최대 hp, mp가 각 인스턴스 별로 있지 않도록 static 키워드 추가
+  static const int maxHp = 50;
+  static const int maxMp = 10;
+
   String name;
-  int hp;
-  int mp;
+  int hp = 50;
+  int mp = 10;
 
-  final int maxHp = 50;
-  final int maxMp = 10;
-
-  Cleric(this.name, this.hp, this.mp);
+  Cleric(
+    this.name, {
+    this.hp = maxHp,
+    this.mp = maxMp,
+  });
 
   ///   성직자는 셀프 에이드라는 마법을 사용할 수 있고 mp를 5소비하면 자신의 hp를 최대hp까지 회복할 수 있다.
   ///   selfAid() 인수 x, 리턴 x
@@ -20,8 +27,10 @@ class Cleric {
     if (mp >= 5) {
       mp -= 5;
       hp = maxHp;
-      print('$name는 mp를 5만큼 사용해 hp를 최대로 회복했다.');
-      print('현재 mp : $mp\n');
+      print('$name는 mp를 5만큼 사용해 hp를 최대로 회복했다.\n');
+
+      print('$name의 상태');
+      print('hp : $hp, mp : $mp\n');
     } else {
       print('$name는 mp의 부족으로 hp 회복에 실패했다.');
     }
@@ -32,21 +41,39 @@ class Cleric {
   ///   3초 기도하면 회복량은 3~5포인트 최대 mp보다 더 회복하는 것은 불가능
   ///   pray(int time) 인수에 기도할 시간을 지정할 수 있다. 리턴값은 실제로 회복된 mp 양을 반환한다
   int pray(int time) {
-
-    int maxRecovery = maxMp - mp; // 현재 mp에서 회복 가능한 양
+    int mpRecovery = maxMp - mp; // 현재 mp에서 회복 가능한 양
+    int sumMp = 0; // 기도시간 + mp 회복량의 합을 저장핣 변수
 
     // 랜덤 mp 회복량 0~2포인트, addMp < 3
     int addMp = Random().nextInt(3);
 
-    // 총 회복량
-    // 기도 시간과 mp 회복량의 합과 현재 mp의 최대 회복 가능한 양 중 작은 값을 선택
-    int sumMp = min(time + addMp, maxRecovery);
+    if (time == 0) {
+      sumMp = 0;
+      addMp = 0;
 
-    // mp를 업데이트
-    mp = mp + sumMp;
+      // 총 회복량
+      // 기도 시간과 mp 회복량의 합과 현재 mp의 최대 회복 가능한 양 중 작은 값을 선택
+      sumMp = min(time + addMp, mpRecovery);
+
+      // mp를 업데이트
+      mp = mp + sumMp;
+
+      print('$name의 기도가 실패했다.');
+      print(
+          '$name - 기도시간 : $time, mp 회복량 : $addMp, 총 mp 회복량 : $sumMp, 현재 mp : $mp\n');
+
+      return sumMp;
+    } else {
+      // 총 회복량
+      // 기도 시간과 mp 회복량의 합과 현재 mp의 최대 회복 가능한 양 중 작은 값을 선택
+      sumMp = min(time + addMp, mpRecovery);
+
+      // mp를 업데이트
+      mp = mp + sumMp;
+    }
 
     print(
-        '$name - 기도시간 : $time, mp 회복량 : $addMp, 총 mp 회복량 : $sumMp, 현재 mp : $mp');
+        '$name - 기도시간 : $time, mp 회복량 : $addMp, 총 mp 회복량 : $sumMp, 현재 mp : $mp\n');
 
     return sumMp; // 총 회복량 반환
   }
--- lib/11_30/main.dart
+++ lib/11_30/main.dart
@@ -2,10 +2,25 @@ import 'cleric.dart';
 import 'dart:math';
 
 void main() {
-  Cleric cleric = Cleric('성직자', 50, 10);
+  final Cleric cleric = Cleric('성직자', hp: 50, mp: 10);
 
   cleric.selfAid(); // 셀프 에이드
 
-  int time = Random().nextInt(5) + 1; // 기도시간 랜덤으로 생성 1~5
+  final int time = Random().nextInt(5); // 기도시간 랜덤으로 생성 0~5
   cleric.pray(time); // 기도하기
+
+  /// A. 이름, hp, mp를 지정하여 인스턴스화
+  final Cleric a = Cleric('아서스', hp: 40, mp: 5);
+  print('A \nname : ${a.name}, hp : ${a.hp}, mp : ${a.mp}\n');
+
+  /// B. 이름과 hp만 지정하여 인스턴스화, mp는 최대mp와 같은 값으로 초기화
+  final Cleric b = Cleric('아서스', hp: 35);
+  print('B \nname : ${b.name}, hp : ${b.hp}, mp : ${b.mp}\n');
+
+  /// C. 이름만 지정하여 인스턴스화, hp, mp는 최대 hp와 최대 mp로 초기화
+  final Cleric c = Cleric('아서스');
+  print('C \nname : ${c.name}, hp : ${c.hp}, mp : ${c.mp}\n');
+
+  /// D. 이름을 지정하지 않는 경우에는 인스턴스화가 불가능함
+  // final Cleric d = Cleric();
 }
