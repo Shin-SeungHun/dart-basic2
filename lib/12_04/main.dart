3914dbd668d48c07adf6de5c73ef6005c1ba7b53
shin-SeungHun <sng7387@gmail.com>
2023-12-04 21:18:20 +0900

    연습문제 2-1

--- lib/12_04/main.dart
+++ lib/12_04/main.dart
@@ -1,6 +1,14 @@
 import 'package:basic/12_04/wand.dart';
 import 'package:basic/12_04/wizard.dart';
 
+// 연습문제 2-1
+// 1. 대한민국의 도시 이름 모음 (순서 상관 없음)
+//     - list
+// 2. 10명 학생의 시험 점수
+//     - map
+// 3. 대한민국의 도시별 인구수 (순서 상관 없음)
+//     - map
+
 void main() {
   final Wand wand = Wand(name: '딱총나무 지팡이', power: 10);
   // wand.name = 'dd'; error
