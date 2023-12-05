c3467305037fdf61589fe6c23e5817fc17edebed
shin-SeungHun <sng7387@gmail.com>
2023-12-06 01:00:37 +0900

    무형 자산 리스트 추가, 무형 자산 추가 메서드, 무형 자산 목록 메서드 추가

--- lib/23_12_05/abstract&interface/intangible_asset.dart
+++ lib/23_12_05/abstract&interface/intangible_asset.dart
@@ -1,5 +1,39 @@
 import 'asset.dart';
+
 /// 회사에서 형태가 없는 무형자산(IntangibleAsset)도 관리하려고 생각하고 있다.
 /// 무형자산에는, 예를 들어 특허권(Patent)등이 있다.
 /// 무형자산은 자산(Asset)의 일종이다.
-abstract class IntangibleAsset extends Asset {}
+abstract class IntangibleAsset extends Asset {
+  List<IntangibleAsset> intangibleAssetList = [];
+
+  IntangibleAsset({required super.assetList});
+
+  void intangibleAssetAdd() {
+    print('무형 자산 추가');
+    intangibleAssetList.add(this);
+  }
+
+  void intangibleAsset() {
+    print('무형 자산 목록');
+    for (IntangibleAsset intangibleAsset in intangibleAssetList) {
+      print(intangibleAsset);
+    }
+  }
+
+  @override
+  void assetAdd() {
+    super.assetList.add(this);
+  }
+
+  @override
+  void asset() {
+    for (Asset asset in super.assetList) {
+      print(asset);
+    }
+  }
+
+  @override
+  String toString() {
+    return 'IntangibleAsset{intangibleAssetList: $intangibleAssetList}';
+  }
+}
