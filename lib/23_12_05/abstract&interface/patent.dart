302b3283d24b75e7f1806c906b7187e3e4af4ff8
shin-SeungHun <sng7387@gmail.com>
2023-12-05 16:20:06 +0900

    연습문제 12-2 다. 필드 추가

--- lib/23_12_05/abstract&interface/patent.dart
+++ lib/23_12_05/abstract&interface/patent.dart
@@ -1,3 +1,16 @@
 import 'intangible_asset.dart';
 
-class Patent extends IntangibleAsset {}
+class Patent extends IntangibleAsset {
+  String name;
+  int price;
+
+  Patent({
+    required this.name,
+    required this.price,
+  });
+
+  @override
+  String toString() {
+    return 'Patent{name: $name, price: $price}';
+  }
+}
