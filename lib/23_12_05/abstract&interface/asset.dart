7676fffdd8773e20f43a5ca0ef04146fe51cdbfa
shin-SeungHun <sng7387@gmail.com>
2023-12-06 00:58:42 +0900

    asset에 자산목록 담을 list 추가 자산 추가하는 메서드, 자산 목록 메서드 추가

--- lib/23_12_05/abstract&interface/asset.dart
+++ lib/23_12_05/abstract&interface/asset.dart
@@ -1,2 +1,23 @@
 /// 자산
-abstract class Asset {}
+abstract class Asset {
+  List<Asset> assetList = [];
+
+  Asset({required this.assetList});
+
+  void assetAdd() {
+    print('자산 추가');
+    assetList.add(this);
+  }
+
+  void asset() {
+    print('자산 목록');
+    for (Asset asset in assetList) {
+      print(asset);
+    }
+  }
+
+  @override
+  String toString() {
+    return 'Asset{assetLists: $assetList}';
+  }
+}
