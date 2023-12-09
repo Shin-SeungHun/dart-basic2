98a3d07cc66d0d2da065a0b18c201a794e2a6fab
shin-SeungHun <sng7387@gmail.com>
2023-12-09 12:44:03 +0900

    book 클래스 hashcode 수정

--- lib/23_12_07/book.dart
+++ lib/23_12_07/book.dart
@@ -39,13 +39,14 @@ class Book {
       other is Book &&
           runtimeType == other.runtimeType &&
           _title == other._title &&
-          DateFormat('yyyy-MM--dd').format(_publishDate) ==
-              DateFormat('yyyy-MM-dd').format(other._publishDate);
+          _publishDate.isAtSameMomentAs(other._publishDate); // 문자열로 변환하는 대신 DateTime의 객체를 직접 비교하는 isAsSameMomentsAs()메서도 사용
+          // DateFormat('yyyy-MM--dd').format(_publishDate) ==
+          //     DateFormat('yyyy-MM-dd').format(other._publishDate);
 
   @override
   int get hashCode =>
-      _title.hashCode ^ DateFormat('yyyy-MM-dd').format(_publishDate).hashCode;
-
+      _title.hashCode ^_publishDate.hashCode;
+      // DateFormat('yyyy-MM-dd').format(_publishDate).hashCode;
   /// 깊은 복사
   Book copyWith({
     String? title,
