8e3c59963e9732badbc3aecda3349fb920e9c2ce
shin-SeungHun <sng7387@gmail.com>
2023-12-11 17:34:02 +0900

    코드 정렬

--- lib/23_12_11/data_format/main.dart
+++ lib/23_12_11/data_format/main.dart
@@ -9,11 +9,14 @@ void main() {
   Department department = Department(name: '총무부', leader: leader);
 
   Map<String, dynamic> company = department.toJson();
+  print('$company\n');
+
   // JSON 형식의 문자열로 변환
-  String jsonString = jsonEncode(company);
+  String json = jsonEncode(company);
+  print('$json\n');
 
   File companyFile = File('company.txt');
-  companyFile.writeAsStringSync(jsonString);
+  companyFile.writeAsStringSync(json);
 
   print('데이터 직렬화 완료, company.txt파일 생성 완료');
 
