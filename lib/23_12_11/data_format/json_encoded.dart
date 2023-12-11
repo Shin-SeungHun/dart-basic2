3ee4dad44213553ad7838805f85296ac3a03925d
shin-SeungHun <sng7387@gmail.com>
2023-12-11 17:42:52 +0900

    코드 정렬 예외처리 추가

--- lib/23_12_11/data_format/json_encoded.dart
+++ lib/23_12_11/data_format/json_encoded.dart
@@ -86,18 +86,27 @@ class Department {
 } // class Department
 
 void main() {
-  final Employee leader = Employee(name: '홍길동', age: 41);
-  final Department department = Department(name: '총무과', leader: leader);
+  try {
+    final Employee leader = Employee(name: '홍길동', age: 41);
+    final Department department = Department(name: '총무과', leader: leader);
 
-  Map<String, dynamic> company = department.toJson();
-  print('$company\n');
-  String json = jsonEncode(company);
-  print('$json\n');
+    // 직렬화: Map으로 변환
+    final Map<String, dynamic> company = department.toJson();
+    print('$company\n');
 
-  File companyFile = File('company.txt');
-  companyFile.writeAsStringSync(json);
-  print('데이터 직렬화 완료, company.txt파일 생성 완료');
+    // JSON 형식의 문자열로 변환
+    final String json = jsonEncode(company);
+    print('$json\n');
 
-  String result = companyFile.readAsStringSync();
-  print(result);
+    // 파일 쓰기
+    final File companyFile = File('company.txt');
+    companyFile.writeAsStringSync(json);
+    print('데이터 직렬화 완료, company.txt파일 생성 완료');
+
+    // 파일 읽기
+    final String result = companyFile.readAsStringSync();
+    print(result);
+  } catch (e) {
+    print('error: $e');
+  }
 } // main()
--- lib/23_12_11/data_format/main.dart
+++ lib/23_12_11/data_format/main.dart
@@ -5,21 +5,22 @@ import 'department.dart';
 import 'employee.dart';
 
 void main() {
-  Employee leader = Employee(name: '홍길동', age: 41);
-  Department department = Department(name: '총무부', leader: leader);
+  try {
+    final Employee leader = Employee(name: '홍길동', age: 41);
+    final Department department = Department(name: '총무과', leader: leader);
 
-  Map<String, dynamic> company = department.toJson();
-  print('$company\n');
+    final Map<String, dynamic> company = department.toJson();
+    print('$company\n');
+    final String json = jsonEncode(company);
+    print('$json\n');
 
-  // JSON 형식의 문자열로 변환
-  String json = jsonEncode(company);
-  print('$json\n');
+    final File companyFile = File('company.txt');
+    companyFile.writeAsStringSync(json);
+    print('데이터 직렬화 완료, company.txt파일 생성 완료');
 
-  File companyFile = File('company.txt');
-  companyFile.writeAsStringSync(json);
-
-  print('데이터 직렬화 완료, company.txt파일 생성 완료');
-
-  String result = companyFile.readAsStringSync();
-  print(result);
+    final String result = companyFile.readAsStringSync();
+    print(result);
+  } catch (e) {
+    print('error: $e');
+  }
 }
