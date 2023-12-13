3f7da6faec6e97e4bcd407931af25f1de3fd6343
shin-SeungHun <sng7387@gmail.com>
2023-12-13 21:52:30 +0900

    연습2. API호출 및 데이터 처리 제출

---
+++ lib/23_12_13/async_json_api.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+void main() async {
+  String movieInfo = await getMovieInfo();
+  Map<String, dynamic> movieData = jsonDecode(movieInfo);
+  String director = movieData['director'];
+  print('감독: $director'); // George Lucas
+
+  Movie movie = Movie(title: '', director: '', year: 0);
+  movie.title = movieData['title'];
+  movie.director = movieData['director'];
+  movie.year = movieData['year'];
+  print(movie.toString());
+} // void main()
+
+Future<String> getMovieInfo() async {
+  await Future.delayed(Duration(seconds: 2));
+  final mockData = {
+    'title': 'Star Wars',
+    'director': 'George Lucas',
+    'year': 1977,
+  };
+
+  return jsonEncode(mockData);
+} // getMovieInfo()
+
+class Movie {
+  String _title;
+  String _director;
+  int _year;
+
+  Movie({
+    required String title,
+    required String director,
+    required int year,
+  })  : _title = title,
+        _director = director,
+        _year = year;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': _title,
+      'director': _director,
+      'year': _year,
+    };
+  }
+
+  factory Movie.fromJson(Map<String, dynamic> map) {
+    return Movie(
+      title: map['title'] as String,
+      director: map['director'] as String,
+      year: map['year'] as int,
+    );
+  }
+
+  int get year => _year;
+
+  set year(int value) {
+    _year = value;
+  }
+
+  String get director => _director;
+
+  set director(String value) {
+    _director = value;
+  }
+
+  String get title => _title;
+
+  set title(String value) {
+    _title = value;
+  }
+
+  @override
+  String toString() {
+    return 'Movie{title: $_title, director: $_director, year: $_year}';
+  }
+} // Movie
