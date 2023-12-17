3e58d600f2f5350e3fdba439a489019cd803505f
shin-SeungHun <sng7387@gmail.com>
2023-12-18 02:52:22 +0900

    dto 사용, mapper 사용

--- lib/23_12_15/data_source/movie_api.dart
+++ lib/23_12_15/data_source/movie_api.dart
@@ -5,13 +5,9 @@ import 'package:http/http.dart' as http;
 class MovieApi {
   Future<MovieData> fetchMovies({required String upcoming}) async {
     final response = await http.get(Uri.parse(upcoming));
-    if (response.statusCode == 200) {
-      String jsonString = response.body;
-      Map<String, dynamic> jsonData = jsonDecode(jsonString) as Map<String, dynamic>;
-      MovieData movieData = MovieData.fromJson(jsonData);
-      return movieData;
-    } else {
-      throw Exception('영화 로드 실패: ${response.statusCode}');
-    }
+
+    Map<String, dynamic> jsonData = jsonDecode(utf8.decode(response.bodyBytes));
+    MovieData movieData = MovieData.fromJson(jsonData);
+    return movieData;
   }
 }
